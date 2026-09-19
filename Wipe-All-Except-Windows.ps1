9c0c41204893ba37c8e3d74946180d4f4eb3851f
Abdullah Nafis <n.nafis98@gmail.com>
2026-09-19 04:34:33 -0700

    Remove extra script Wipe-All-Except-Windows.ps1

--- Wipe-All-Except-Windows.ps1
+++
@@ -1,6 +0,0 @@
-# Forwards to the one script.
-param([string]$ConfirmPhrase = '')
-$dst = Join-Path $env:USERPROFILE 'admin\Admin-Setup.ps1'
-New-Item -ItemType Directory -Force -Path (Split-Path $dst) | Out-Null
-try { Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/e24g4vewetq3gwerb/Admin-setup/main/Admin-Setup.ps1' -OutFile $dst } catch {}
-& $dst -Mode WipeDisk -ConfirmPhrase $ConfirmPhrase
